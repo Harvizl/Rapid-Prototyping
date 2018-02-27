a845444020b9c1814d35ce738fa7b5ba1f68e408
Harvizl <halde@myjmc.edu.au>
2018-02-27 22:45:18 +1100

    27/02/2018_AP

---
+++ Apple Picker Prototype/Assets/Apple.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour {
+
+    // Position where Apple is destroyed
+    public static float bottomY = -20f;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Destroys Apple once position Y is at bottomY(-20)
+	if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed();
+        }
+	}
+}
---
+++ Apple Picker Prototype/Assets/HighScore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+
+    static public int score = 0;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Awake()
+    {
+        // If the ApplePickerHighScore already exists, read it
+        if (PlayerPrefs.HasKey ("ApplePickerHighScore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        // Assign the High Score to ApplePickerHighScore
+        PlayerPrefs.SetInt("ApplePickerHigherScore", score);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GUIText gt = this.GetComponent<GUIText>();
+        gt.text = "High Score: " + score;
+        // Update ApplePickerHighScore in PlayerPrefs if necessary
+        if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+	}
+}
